2cb49ecd2357645a485d59735103fa6cd8763817
AmyChen <flutteruuu@gmail.com>
2025-09-07 14:17:02 +0800

    Make a list with ListTitle and icon

--- lib/main.dart
+++ lib/main.dart
@@ -29,32 +29,41 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView(
       children: <Widget>[
-        ListTile(title: Text("我是一個列表")), // 列表項目
+        ListTile(
+          leading: Icon(Icons.home), // 列表圖標
+          title: Text("首頁")
+          ), // 列表項目
         Divider(), // 分隔線
-        ListTile(title: Text("我是一個列表")),
+        ListTile(
+          leading: Icon(Icons.assignment, color: Colors.red), // 列表圖標
+          title: Text("全部訂單")
+          ), // 列表項目
         Divider(), // 分隔線
-        ListTile(title: Text("我是一個列表")),
+        ListTile(
+          leading: Icon(Icons.payment, color: Colors.green), // 列表圖標
+          title: Text("待付款")
+          ), // 列表項目
         Divider(), // 分隔線
-        ListTile(title: Text("我是一個列表")),
+        ListTile(
+          leading: Icon(Icons.local_car_wash, color: Colors.orange), // 列表圖標
+          title: Text("待收貨")
+          ), // 列表項目
         Divider(), // 分隔線
-        ListTile(title: Text("我是一個列表")),
+
+        ListTile(
+          leading: Icon(Icons.favorite, color: Colors.lightGreen), // 列表圖標
+          title: Text("我的收藏")
+          ), // 列表項目
         Divider(), // 分隔線
-        ListTile(title: Text("我是一個列表")),
+
+        ListTile(
+          leading: Icon(Icons.people, color: Colors.black54), // 列表圖標
+          title: Text("在線客服"),
+          trailing: Icon(Icons.chevron_right_sharp),
+          ), // 列表項目
         Divider(), // 分隔線
-        ListTile(title: Text("我是一個列表")),
-        Divider(), // 分隔線
-        ListTile(title: Text("我是一個列表")),
-        Divider(), // 分隔線
-        ListTile(title: Text("我是一個列表")),
-        Divider(), // 分隔線
-        ListTile(title: Text("我是一個列表")),
-        Divider(), // 分隔線
-        ListTile(title: Text("我是一個列表")),
-        Divider(), // 分隔線
-        ListTile(title: Text("我是一個列表")),
-        Divider(), // 分隔線
-        ListTile(title: Text("我是一個列表")),
-        Divider(), // 分隔線
+
+
       ],
     );
   }
