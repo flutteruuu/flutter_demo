d08ac3855b009a6c07cee8486060ff5a35a890ff
AmyChen <flutteruuu@gmail.com>
2025-09-07 16:49:15 +0800

    Scrollable horizontal list

--- lib/main.dart
+++ lib/main.dart
@@ -27,10 +27,70 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: <Widget>[
+    return SizedBox(
+      height: 120,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        padding: const EdgeInsets.all(10),
+        children: <Widget>[
+        Container(
+          width: 120,
+          decoration: const BoxDecoration( color: Colors.red,),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 80,
+                child: Image.network("https://cdn2.ettoday.net/images/8389/8389401.png", fit: BoxFit.cover,)
+              ),
+              const Text("文字"),
+            ],
+          ),
+        ),
 
-      ],
+        Container(
+          width: 120,
+          decoration: const BoxDecoration(
+            color: Colors.orange,
+          ),
+        ),
+
+        Container(
+          width: 120,
+          decoration: const BoxDecoration(
+            color: Colors.yellow,
+          ),
+        ),
+
+        Container(
+          width: 120,
+          decoration: const BoxDecoration(
+            color: Colors.green,
+          ),
+        ),
+
+        Container(
+          width: 120,
+          decoration: const BoxDecoration(
+            color: Colors.blue,
+          ),
+        ),
+
+        Container(
+          width: 120,
+          decoration: const BoxDecoration(
+            color: Colors.purple,
+          ),
+        ),
+
+        Container(
+          width: 120,
+          decoration: const BoxDecoration(
+            color: Colors.pink,
+          ),
+        ),
+
+        ],
+      ),
     );
   }
 }
