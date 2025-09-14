efec583bc289ee0c2452dce2bdc80b0995946b94
AmyChen <flutteruuu@gmail.com>
2025-09-14 15:59:13 +0800

    Initial main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -27,37 +27,10 @@ class HomePage extends StatelessWidget {
 
   const HomePage({super.key});
 
-  Widget _initGridViewData(context, index){
- return  Container(
-            alignment: Alignment.center,
-            decoration:  BoxDecoration(
-              border: Border.all(color: Colors.grey)
-            ),
-            child: Column(
-              children: [
-                Image.network(listData[index]["imgurl"]),
-                const SizedBox(height: 10),
-                Text(listData[index]["title"], style: const TextStyle(
-                  fontSize: 16,
-                ))
-              ],
-            ),
-          );
-  }
-
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      padding: const EdgeInsets.all(10),
-      itemCount: listData.length,
-      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-      crossAxisSpacing: 10, // 水平間距
-      mainAxisSpacing: 10,  // 垂直間距
-      childAspectRatio: 0.8, // 寬高比
-      maxCrossAxisExtent: 200,
-       ),
-
-      itemBuilder: _initGridViewData
+    return const Text(
+      "Hello Flutter"
     );
   }
 }
