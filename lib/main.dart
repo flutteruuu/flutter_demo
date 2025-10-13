68ad4c71b87dc14833269c28a830e7dafc46d6bf
AmyChen <flutteruuu@gmail.com>
2025-10-13 22:51:27 +0800

    Alignment 控制子元素

--- lib/main.dart
+++ lib/main.dart
@@ -29,31 +29,14 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // 獲取螢幕寬度和高度
-    final size = MediaQuery.of(context).size;
 
-    return Stack(
-      children:[
-        ListView(
-          padding: const EdgeInsets.only(top: 50),
-          children: [
-            for (var i = 0; i < 30; i++)
-              ListTile(title: Text("我是一個列表$i")),
-          ],
-        ),
-        Positioned(
-          left: 0,
-          top: 0,
-          width: size.width, // 配置子元素的寬度和高度 不能使用double.infinity
-          height: 44,        // 配置子元素的寬度和高度
-          child: Container(
-            alignment: Alignment.center,
-            height: 44,
-            color: Colors.black,
-            child: const Text("二級導航", style: TextStyle(color: Colors.white),),
-              ))
-        
-      ],
+    return Container(
+      width: 200,
+      height: 200,
+      color: Colors.red,
+      child: const Align(
+        alignment: Alignment.center,
+        child: Text("Hello Flutter"),),
     );
-    }
+  }
 }
