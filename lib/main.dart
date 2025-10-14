c08df1a0a89455704bcc33012e828b2caf8bb308
AmyChen <flutteruuu@gmail.com>
2025-10-14 22:41:36 +0800

    Card Sample Card 實現一個通訊錄的卡片

--- lib/main.dart
+++ lib/main.dart
@@ -30,11 +30,51 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // 需求: 頁面上顯示一個容器, 寬度是螢幕寬度, 高度是寬度的一半
-    return AspectRatio(
-      aspectRatio: 2/1,
-      child: Container(
-        color: Colors.red,
-      ),
+    return ListView(
+      children:[
+        
+        Card(
+          shape: RoundedRectangleBorder( //設定圓角 Card的陰影效果
+            borderRadius: BorderRadius.circular(20),
+          ),
+          elevation: 10,
+          margin: EdgeInsets.all(10),
+          child: Column(
+            children: const[
+              ListTile(
+                title: Text("張三", style: TextStyle(fontSize: 28),),
+                subtitle: Text("軟體工程師"),
+              ),
+              Divider(),
+              ListTile(
+                title: Text("電話: 123456789"),
+              ),
+              ListTile(
+                title: Text("地址: 台北市中正區"),
+              ),
+            ],)
+        ),
+        
+        Card(
+          elevation: 10,
+          margin: EdgeInsets.all(10),
+          // color: Colors.black12,  // Card的背景顏色
+          child: Column(
+            children: const[
+              ListTile(
+                title: Text("李四", style: TextStyle(fontSize: 28),),
+                subtitle: Text("Flutter軟體工程師"),
+              ),
+              Divider(),
+              ListTile(
+                title: Text("電話: 123456789"),
+              ),
+              ListTile(
+                title: Text("地址: 台北市中正區"),
+              ),
+            ],)
+        )
+      ]
     );
   }
 }
