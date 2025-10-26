c97c55045eab06eb6a3d0a82e65c389dc3a13347
AmyChen <flutteruuu@gmail.com>
2025-10-26 20:12:19 +0800

    使用Wrap組件實現電商App搜索頁面布局

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import './res/listData.dart';
 
-
 void main() {
-  runApp( const MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -12,9 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Flutter DEMO",
-      theme: ThemeData(
-        primarySwatch: Colors.teal,
-      ),
+      theme: ThemeData(primarySwatch: Colors.teal),
       home: Scaffold(
         appBar: AppBar(title: const Text("Flutter App")),
         body: const HomePage(),
@@ -24,36 +21,63 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
-
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    
-    return Padding(
-      padding: EdgeInsets.all(10),
-      child: Wrap(
-        alignment: WrapAlignment.spaceAround,
-        spacing: 10, // 水平間距
-        runSpacing: 10, // 垂直間距
-        direction: Axis.horizontal,
-        children: [
-          Button("第1集", onPressed: (){}),
-          Button("第2集", onPressed: (){}),
-          Button("第3集", onPressed: (){}),
-          Button("第4集", onPressed: (){}),
-          Button("第5集", onPressed: (){}),
-          Button("第6集", onPressed: (){}),
-          Button("第7集", onPressed: (){}),
-          Button("第8集", onPressed: (){}),
-          Button("第9集", onPressed: (){}),
-          Button("第10集", onPressed: (){}),
-          Button("第11集", onPressed: (){}),
-          Button("第12集", onPressed: (){}),
-          Button("第13集", onPressed: (){}),
-          Button("第14集", onPressed: (){}),
-        ] 
-      ),
+    return ListView(
+      padding: const EdgeInsets.all(10),
+      children: [
+        Row(
+          children: [Text("熱搜", style: Theme.of(context).textTheme.titleLarge)],
+        ),
+        const Divider(),
+        Wrap(
+          spacing: 10, // 水平間距
+          runSpacing: 10, // 垂直間距
+          children: [
+            Button("女裝", onPressed: () {}),
+            Button("筆記本", onPressed: () {}),
+            Button("文具", onPressed: () {}),
+            Button("書", onPressed: () {}),
+            Button("電子產品", onPressed: () {}),
+            Button("時尚", onPressed: () {}),
+            Button("襪子", onPressed: () {}),
+            Button("上衣", onPressed: () {}),
+          ],
+        ),
+        SizedBox(height: 10),
+        Row(
+          children: [
+            Text("歷史紀錄", style: Theme.of(context).textTheme.titleLarge),
+          ],
+        ),
+        const Divider(),
+        Column(
+          children: [
+            ListTile(title: Text("女裝")),
+            Divider(),
+            ListTile(title: Text("筆記本")),
+            Divider(),
+            ListTile(title: Text("文具")),
+          ],
+        ),
+        const SizedBox(height: 10),
+        Padding(
+          padding: EdgeInsets.all(40),
+          child:
+            OutlinedButton.icon(
+              // 自適應
+              style: ButtonStyle(
+                foregroundColor: WidgetStateProperty.all(Colors.black45),
+              ),
+              onPressed: () {},
+              icon: Icon(Icons.delete),
+              label: Text("清除歷史紀錄"),
+            ),
+          
+        ),
+      ],
     );
   }
 }
@@ -61,19 +85,19 @@ class HomePage extends StatelessWidget {
 // 自訂義按鈕組件
 class Button extends StatelessWidget {
   String text; // 按鈕的文字
-  void Function()? onPressed;  // 方法
+  void Function()? onPressed; // 方法
   Button(this.text, {super.key, required this.onPressed});
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
       style: ButtonStyle(
-        backgroundColor: WidgetStateProperty.all(Color.fromARGB(255, 204, 225, 240)),
+        backgroundColor: WidgetStateProperty.all(
+          Color.fromARGB(255, 204, 225, 240),
+        ),
         foregroundColor: WidgetStateProperty.all(Colors.black45),
       ),
-      onPressed: () {
-        
-      },
+      onPressed: () {},
       child: Text(text),
     );
   }
