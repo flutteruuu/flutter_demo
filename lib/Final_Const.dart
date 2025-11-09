4de9fc8afc9f45ad06ca1491b4fb0ba7fbebd700
AmyChen <flutteruuu@gmail.com>
2025-11-09 19:23:59 +0800

    1. 按新增按鈕，動態列表增加  2. const和final的定義

---
+++ lib/Final_Const.dart
@@ -0,0 +1,20 @@
+void main() {
+
+  // const 常量在編譯時期就確定下來，不能在運行時期改變
+  // final 常量在運行時期才能確定下來，但一旦賦值後就不能再改變(只能賦值一次)
+  //const PI = 3.14;
+  //final A = 3.15;
+
+  //final B;
+  //B = 123;
+  //B = 456; // 錯誤: final 變量只能賦值一次
+  //print(B);
+
+  final List l = [];
+
+  l.add(12);
+  l.add(22);
+  l.add(32);
+  print(l);
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -30,25 +30,27 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final List<String> _list = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Flutter App")),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: (){
+          // 改變數據必須加上setState
+          setState(() {
+            _list.add("我是一個新增列表");
+          });
+          // 點擊事件
+        },
+      ),
       body: ListView(
-        children: const [
-          ListTile(
-            title: Text("我是一個列表"),
-          ),
-          ListTile(
-            title: Text("我是一個列表"),
-          ),
-          ListTile(
-            title: Text("我是一個列表"),
-          ),
-          ListTile(
-            title: Text("我是一個列表"),
-          ),
-        ],
+        children: _list.map((v) { // map 方法會遍歷 _list 中的每個元素，並將每個元素（v）轉換為一個 ListTile 小部件
+          return ListTile(
+            title: Text(v),
+          );
+        }).toList(),
       ),
     );
   }
